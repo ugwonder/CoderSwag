ec0528057ea1468b7f306028a8382ff58077ebd7
mgbachi_ugo <ugochukwu_mgbachi@yahoo.com>
2019-11-29 02:41:12 +0100

    lesson 02

--- app/src/main/java/com/mgbachi_ugo/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/mgbachi_ugo/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mgbachi_ugo.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.mgbachi_ugo.coderswag.Adapter.CategoryAdapter
 import com.mgbachi_ugo.coderswag.Model.Category
 import com.mgbachi_ugo.coderswag.R
 import com.mgbachi_ugo.coderswag.Services.DataService
@@ -10,15 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
-            DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/com/mgbachi_ugo/coderswag/Services/DataService.kt
+++ app/src/main/java/com/mgbachi_ugo/coderswag/Services/DataService.kt
@@ -8,6 +8,14 @@ object DataService {
         Category("SHIRTS", "shirtimage"),
         Category("HOODIES", "hoodieimage"),
         Category("HATS", "hatimage"),
+        Category("DIGITAL", "digitalgoodsimage"),
+        Category("SHIRTS", "shirtimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS", "hatimage"),
+        Category("DIGITAL", "digitalgoodsimage"),
+        Category("SHIRTS", "shirtimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS", "hatimage"),
         Category("DIGITAL", "digitalgoodsimage")
     )
      val hats = listOf(
